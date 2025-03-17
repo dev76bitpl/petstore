e0402b5bd94c6f234124bc85082de955b519e984
cdue <inf@cdue.edu.pl>
2025-03-17 23:04:21 +0100

    Add routes for app

--- PetApp/routes/web.php
+++ PetApp/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PetController;
+
+Route::get('/pets', [PetController::class, 'index'])->name('pets.index');
+Route::get('/pets/create', [PetController::class, 'create'])->name('pets.create');
+Route::post('/pets', [PetController::class, 'store'])->name('pets.store');
+Route::get('/pets/{id}', [PetController::class, 'show'])->name('pets.show'); // ✅ Dodana trasa
+Route::get('/pets/{id}/edit', [PetController::class, 'edit'])->name('pets.edit');
+Route::put('/pets/{id}', [PetController::class, 'update'])->name('pets.update');
+Route::delete('/pets/{id}', [PetController::class, 'destroy'])->name('pets.destroy');
+
 Route::get('/', function () {
     return view('welcome');
 });
